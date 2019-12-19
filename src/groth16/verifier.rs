984ed0941008b3c4b6a2d22b984a1d2dbf24ba47
dignifiedquire <me@dignifiedquire.com>
2019-12-19 14:41:11 +0100

    feat: basic impl of batch verification, thanks @arielgabizon

--- src/groth16/verifier.rs
+++ src/groth16/verifier.rs
@@ -1,4 +1,4 @@
-use ff::PrimeField;
+use ff::{Field, PrimeField};
 use groupy::{CurveAffine, CurveProjective};
 use paired::{Engine, PairingCurveAffine};
 
@@ -52,4 +52,87 @@ pub fn verify_proof<'a, E: Engine>(
     ))
     .unwrap()
         == pvk.alpha_g1_beta_g2)
+}
+
+// randomized batch verification - see Appendix B.2 in Zcash spec
+pub fn verify_proofs<'a, E: Engine, R: rand::RngCore>(
+    pvk: &'a PreparedVerifyingKey<E>,
+    rng: &mut R,
+    proofs: &[Proof<E>],
+    public_inputs: &[&[E::Fr]],
+) -> Result<bool, SynthesisError>
+where
+    E::Fr: AsRef<[u64]>,
+    <<E as ff::ScalarEngine>::Fr as ff::PrimeField>::Repr: From<<E as ff::ScalarEngine>::Fr>,
+{
+    for pub_input in public_inputs {
+        if (pub_input.len() + 1) != pvk.ic.len() {
+            return Err(SynthesisError::MalformedVerifyingKey);
+        }
+    }
+
+    let PI_num = pvk.ic.len() - 1;
+    let proof_num = proofs.len();
+
+    // choose random coefficients for combining the proofs
+    let mut r = vec![];
+    for _ in 0..proof_num {
+        r.push(E::Fr::random(rng));
+    }
+
+    // create corresponding scalars for public input vk elements
+    let mut PI_scalars = vec![];
+
+    for i in 0..PI_num {
+        PI_scalars.push(E::Fr::zero());
+        for j in 0..proof_num {
+            let mut tmp = r[j];
+            tmp.mul_assign(&public_inputs[j][i]);
+            PI_scalars[i].add_assign(&tmp);
+        }
+    }
+
+    // create group element corresponding to public input combination
+    // This roughly corresponds to Accum_Gamma in spec
+    let mut acc_PI = pvk.ic[0].into_projective();
+
+    for (i, b) in PI_scalars.iter().zip(pvk.ic.iter().skip(1)) {
+        acc_PI.add_assign(&b.mul(i.into_repr()));
+    }
+
+    // TODO: why is this not used?
+    let acc_PI = acc_PI.into_affine().prepare();
+
+    let mut sum_r = E::Fr::zero();
+    for i in r.iter() {
+        sum_r.add_assign(i);
+    }
+    sum_r.negate();
+    let acc_Y = pvk.alpha_g1_beta_g2.pow(&sum_r);
+
+    // This corresponds to Accum_Delta
+    let mut acc_C = E::G1::zero();
+    for (rand_coeff, proof) in r.iter().zip(proofs.iter()) {
+        let mut tmp: E::G1 = proof.c.into();
+        tmp.mul_assign(*rand_coeff);
+        acc_C.add_assign(&tmp);
+    }
+
+    let acc_C = acc_C.into_affine().prepare();
+
+    let mut ML_G1 = vec![];
+    let mut ML_G2 = vec![];
+    for (rand_coeff, proof) in r.into_iter().zip(proofs.iter()) {
+        let mut tmp: E::G1 = proof.a.into();
+        tmp.mul_assign(rand_coeff);
+        ML_G1.push(tmp.into_affine().prepare());
+        let mut tmp: E::G2 = proof.b.into();
+        tmp.negate();
+        ML_G2.push(tmp.into_affine().prepare());
+    }
+    let parts = ML_G1.iter().zip(ML_G2.iter()).collect::<Vec<_>>();
+    let mut acc_AB = E::miller_loop(&parts);
+    acc_AB.mul_assign(&E::miller_loop(&[(&acc_C, &pvk.neg_delta_g2)]));
+
+    Ok(E::final_exponentiation(&acc_AB).unwrap() == acc_Y)
 }
